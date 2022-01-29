52109d66b48df6d02df20258cb421a22877e5458
Maxim <maximadda2001@gmail.com>
2022-01-29 16:53:27 +0200

    amazon_s3_manager 1.0.6

--- amazonBuckets/amazonBuckets/settings.py
+++ amazonBuckets/amazonBuckets/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = [str(os.environ.get('ALLOWED_HOSTS'))]
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'amazonBuckets.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('ENGINE'),
+        'NAME': os.environ.get('NAME'), 
+        'USER': os.environ.get('USER'), 
+        'PASSWORD': os.environ.get('PASS'),
+        'HOST': os.environ.get('HOST'), 
+        'PORT': os.environ.get('PORT'),
     }
 }
 
@@ -117,10 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- amazonBuckets/buckets/models.py
+++ amazonBuckets/buckets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Buckets(models.Model):
+    bucket_name = models.CharField("Название бакета", max_length=255, unique=True)
+    region_name = models.CharField("Регион", max_length=255)
+    created = models.CharField("Создан", max_length=40)
+
+class json_files(models.Model):
+    file_name = models.CharField("Название приложения", max_length=255)
+    content = models.JSONField("Содержимое", max_length=255)
+    last_update = models.CharField("Последнее обновление", max_length=40)
+    bucket = models.ForeignKey(Buckets, on_delete = models.CASCADE)
