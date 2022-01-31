d7472192613ef412a30212d32ec5868f95de4e5f
Maxim <maximadda2001@gmail.com>
2022-01-31 03:48:15 +0200

    amazon_s3_manager 1.2.2

--- amazon_s3_manager/urls.py
+++ amazon_s3_manager/urls.py
@@ -6,6 +6,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path(f'{hashlib.sha256("amazonBuckets".encode("utf8")).hexdigest()}/', include('buckets_manager.urls'))
 ]
-
-# print()
-# ffdccf0a7a1db7a7e8b3c453fa43c5e4bc67de6f2a3ba38430590ef773e5ca98
