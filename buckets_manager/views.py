4d5a3aa028810371d9f342bdd6ffc241734470f7
MaxDrg <maximadda2001@gmail.com>
2022-03-31 17:11:13 +0200

    amazon_s3_manager 1.3.1

--- buckets_manager/views.py
+++ buckets_manager/views.py
@@ -211,11 +211,13 @@ def settings(request):
     })
 
 def logs(request):
+    data = models.logs.objects.all().reverse()
+    print (data)
     return render(request, "logs.html", 
     {
         "current_sidebar": "logs", 
         "buckets_names": models.buckets.objects.all(),
-        "logs": models.logs.objects.all().reverse(),
+        "logs": data
     })
 
 def create_file_name(bucket_id: int):
